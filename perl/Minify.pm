a9d33b120f85ae98116b5bef400d8c346aeefd96
Daniel Hobson <denji@localhost>
2013-05-06 13:55:05 +0300

    Revert "Only testing, not production"
    This reverts commit 214909014c6110e0d099f9a081a5b3bed96558ea.

--- perl/Minify.pm
+++ perl/Minify.pm
@@ -6,8 +6,8 @@ use HTML::Packer;
 
 sub html_handler {
     my $r = shift;
-#    my $cache_file = $r->filename;
-#    $cache_file =~ s/.html/.min.html/gi;
+    my $cache_file = $r->filename;
+    $cache_file =~ s/.html/.min.html/gi;
     my $filename = $r->filename;
     local $/=undef;
 
@@ -17,10 +17,9 @@ sub html_handler {
     my $html = <INFILE>;
     close(INFILE);
 
-#    open(OUTFILE, '>' . $cache_file) or die "Error writing file: $!";
-#    print OUTFILE HTML::Packer::minify($html, {  remove_comments => 1,  remove_newlines => 1,  no_compress_comment => 1,  html5 => 1  });
-#    close(OUTFILE);
-    print HTML::Packer::minify($html, {  remove_comments => 1,  remove_newlines => 1,  no_compress_comment => 1,  html5 => 1  });
+    open(OUTFILE, '>' . $cache_file) or die "Error writing file: $!";
+    print OUTFILE HTML::Packer::minify(\$html, {  remove_comments => 1,  remove_newlines => 1,  no_compress_comment => 1,  html5 => 1  });
+    close(OUTFILE);
 
     $r->send_http_header('text/html');
     $r->sendfile($cache_file);
@@ -29,8 +28,8 @@ sub html_handler {
 
 sub css_handler {
     my $r = shift;
-#    my $cache_file = $r->filename;
-#    $cache_file =~ s/.css/.min.css/gi;
+    my $cache_file = $r->filename;
+    $cache_file =~ s/.css/.min.css/gi;
     my $filename = $r->filename;
     local $/=undef;
 
@@ -40,10 +39,9 @@ sub css_handler {
     my $css = <INFILE>;
     close(INFILE);
 
-#    open(OUTFILE, '>' . $cache_file) or die "Error writing file: $!";
-#    print OUTFILE CSS::Minifier::XS::minify($css);
-#    close(OUTFILE);
-    print CSS::Minifier::XS::minify($css);
+    open(OUTFILE, '>' . $cache_file) or die "Error writing file: $!";
+    print OUTFILE CSS::Minifier::XS::minify($css);
+    close(OUTFILE);
 
     $r->send_http_header('text/css');
     $r->sendfile($cache_file);
@@ -52,8 +50,8 @@ sub css_handler {
 
 sub js_handler {
     my $r = shift;
-#    my $cache_file = $r->filename;
-#    $cache_file =~ s/.js/.min.js/gi;
+    my $cache_file = $r->filename;
+    $cache_file =~ s/.js/.min.js/gi;
     my $filename = $r->filename;
     local $/=undef;
 
@@ -63,10 +61,9 @@ sub js_handler {
     my $js = <INFILE>;
     close(INFILE);
 
-#    open(OUTFILE, '>' . $cache_file) or die "Error writing file: $!";
-#    print OUTFILE JavaScript::Minifier::XS::minify($js);
-#    close(OUTFILE);
-    print JavaScript::Minifier::XS::minify($js);
+    open(OUTFILE, '>' . $cache_file) or die "Error writing file: $!";
+    print OUTFILE JavaScript::Minifier::XS::minify($js);
+    close(OUTFILE);
 
     $r->send_http_header('application/javascript');
     $r->sendfile($cache_file);
