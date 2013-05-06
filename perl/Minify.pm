b7eba3a49b76ce290e9505b4956b00b4e79d347b
Daniel Hobson <denji@localhost>
2013-05-06 14:00:50 +0300

    Delete save cache file

--- perl/Minify.pm
+++ perl/Minify.pm
@@ -6,8 +6,6 @@ use HTML::Packer;
 
 sub html_handler {
     my $r = shift;
-    my $cache_file = $r->filename;
-    $cache_file =~ s/.html/.min.html/gi;
     my $filename = $r->filename;
     local $/=undef;
 
@@ -17,19 +15,23 @@ sub html_handler {
     my $html = <INFILE>;
     close(INFILE);
 
-    open(OUTFILE, '>' . $cache_file) or die "Error writing file: $!";
-    print OUTFILE HTML::Packer::minify(\$html, {  remove_comments => 1,  remove_newlines => 1,  no_compress_comment => 1,  html5 => 1  });
-    close(OUTFILE);
-
     $r->send_http_header('text/html');
-    $r->sendfile($cache_file);
+    $r->print(
+        HTML::Packer::minify(
+            \$html,
+            {
+                remove_comments     => 1,
+                remove_newlines     => 1,
+                no_compress_comment => 1,
+                html5               => 1
+            }
+        )
+    );
     return OK;
 }
 
 sub css_handler {
     my $r = shift;
-    my $cache_file = $r->filename;
-    $cache_file =~ s/.css/.min.css/gi;
     my $filename = $r->filename;
     local $/=undef;
 
@@ -39,19 +41,13 @@ sub css_handler {
     my $css = <INFILE>;
     close(INFILE);
 
-    open(OUTFILE, '>' . $cache_file) or die "Error writing file: $!";
-    print OUTFILE CSS::Minifier::XS::minify($css);
-    close(OUTFILE);
-
     $r->send_http_header('text/css');
-    $r->sendfile($cache_file);
+    $r->print(CSS::Minifier::XS::minify(\$css));
     return OK;
 }
 
 sub js_handler {
     my $r = shift;
-    my $cache_file = $r->filename;
-    $cache_file =~ s/.js/.min.js/gi;
     my $filename = $r->filename;
     local $/=undef;
 
@@ -61,12 +57,8 @@ sub js_handler {
     my $js = <INFILE>;
     close(INFILE);
 
-    open(OUTFILE, '>' . $cache_file) or die "Error writing file: $!";
-    print OUTFILE JavaScript::Minifier::XS::minify($js);
-    close(OUTFILE);
-
     $r->send_http_header('application/javascript');
-    $r->sendfile($cache_file);
+    $r->print(JavaScript::Minifier::XS::minify(\$js));
     return OK;
 }
 
